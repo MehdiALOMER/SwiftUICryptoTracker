6afc0499d2a5243458166ba91b8ef40a2a2b5d37
MehdiALOMER <mehdi.alomer98@gmail.com>
2024-03-11 23:17:41 +0300

    Initial Commit

---
+++ SwiftUICrypto/SwiftUICryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICryptoApp.swift
+//  SwiftUICrypto
+//
+//  Created by Mehdi on 11.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
